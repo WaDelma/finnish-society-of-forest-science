5bf15b8af674b356103cb80f87703f82d1dca37f
Delma <delma@del.ma>
2020-03-04 19:30:04 +0200

    Tweaking

--- header.php
+++ header.php
@@ -44,7 +44,9 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<img src="<?php echo get_template_directory_uri() ?>/images/menu.png" width="20vw" height="20vw">
+			</button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
---
+++ images/README.md
@@ -0,0 +1,3 @@
+# menu.png
+Creative Commons (Attribute-Share Alike 3.0 Unported)
+https://www.iconfinder.com/icons/134216/hamburger_lines_menu_icon
